c1eaed6389e77f96c3cb056715f20843ea8755e3
Henrique Santos <henrique.144@outlook.com>
2021-01-11 15:10:17 -0300

    Tap e Pluck

--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, pluck, tap } from 'rxjs/operators';
+
+import { Acao } from './modelo/acoes';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +12,20 @@ export class AcoesService {
   constructor(private httpClient: HttpClient) { }
 
   getAcoes() {
-    return this.httpClient.get<any>('http://localhost:3000/acoes')
+    return this.httpClient
+      .get<any>('http://localhost:3000/acoes')
+      .pipe(tap(valor => console.log(valor)))
+      .pipe(pluck('payload')) // Extrai a propriedade do objeto recebido, passando o nome da prop como string
+      .pipe(map(acoes => acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB))))
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if (acaoA.codigo > acaoB.codigo)
+      return 1
+      
+    if (acaoA.codigo < acaoB.codigo)
+      return -1
+
+    return 0
   }
 }
