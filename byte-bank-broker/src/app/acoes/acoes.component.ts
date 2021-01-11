f8709086d3bb92ee4c7afe97a0423633ac942b54
Henrique Santos <henrique.144@outlook.com>
2021-01-11 15:21:58 -0300

    Async Pipe

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -11,20 +11,10 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent implements OnInit, OnDestroy{
+export class AcoesComponent {
   acoesInput = new FormControl()
-  acoes: Acoes
-  private subscription: Subscription
+  acoes$ = this.acoesService.getAcoes()
 
   constructor(private acoesService: AcoesService) {}
   
-  ngOnInit(): void {
-    this.subscription = this.acoesService.getAcoes().subscribe(acoes => {
-      this.acoes = acoes
-    })
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe()
-  }
 }
